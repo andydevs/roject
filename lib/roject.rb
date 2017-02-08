16bb7988fe8049a6817811f1dfc6d130651cd286
andydevs <akanshul97@gmail.com>
2017-02-08 16:21:35 -0500

    Included date library in Roject

--- lib/roject.rb
+++ lib/roject.rb
@@ -12,7 +12,8 @@ Created: 7 - 8 - 2016
 =end
 
 # Available files
-require_relative "project"
+require 'date'
+require_relative 'project'
 
 # Roject is a programming project manager written in Ruby.
 #
@@ -20,5 +21,5 @@ require_relative "project"
 # Created: 7 - 8 - 2016
 module Roject
 	# The Version of Roject
-	VERSION = "1.0.2"
+	VERSION = "1.0.3"
 end
